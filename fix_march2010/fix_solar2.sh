41fe2a3d0f703fc2edd3dc17de575f6b59e1ced0
Susan Allen <sallen@eos.ubc.ca>
2019-11-29 16:12:19 -0800

    Bash scripts to fix the solar for March 25-28, 2010.
    these are for reference.  they are "hand done"
    
    See also analysis-susan/notebooks/RPN/March2010solar.ipynb

---
+++ fix_march2010/fix_solar2.sh
@@ -0,0 +1,16 @@
+m=3;
+for d in 26 28;
+ do
+ echo $d
+ p=`expr $d - 1`;
+ n=`expr $d + 1`;
+ for h in {000..012};
+  do echo -n "gemlam_y2010m0${m}d${d}_$h.nc: ";
+      /usr/bin/ncra -v solar /data/dlatorne/tmp-rpn-to-gem-lam/gemlam_y2010m0${m}d${p}_$h.nc /data/dlatorne/tmp-rpn-to-gem-lam/gemlam_y2010m0${m}d${n}_$h.nc newfile.nc;
+      cp /data/dlatorne/tmp-rpn-to-gem-lam/gemlam_y2010m0${m}d${d}_$h.nc .
+      ncks -x -v solar gemlam_y2010m0${m}d${d}_$h.nc testfile.nc
+      ncks -A -v solar newfile.nc testfile.nc
+      mv testfile.nc gemlam_y2010m0${m}d${d}_$h.nc
+      rm -f newfile.nc
+  done;
+ done
